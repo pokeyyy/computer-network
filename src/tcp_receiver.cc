63e80a967cc359c98f1ea54e523ec4b8bf0873b2
pokeyyy <1489838471@qq.com>
2024-10-22 00:21:03 +0800

    finish check2

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -17,17 +17,15 @@ void TCPReceiver::receive( TCPSenderMessage message )
     isn_ = message.seqno;
     already_syn = true;
   }
-  uint64_t absolute_seqno = message.seqno.unwrap(isn_, writer().bytes_pushed() + 1);
-  if (!message.payload.empty()) {
-    reassembler_.insert(absolute_seqno + + static_cast<uint64_t>( message.SYN ) - 1, message.payload, message.FIN);
-  }
+  uint64_t absolute_seqno = message.seqno.unwrap(isn_, writer().bytes_pushed());
+  reassembler_.insert(absolute_seqno + uint64_t( message.SYN ) - 1, message.payload, message.FIN);
 }
 
 TCPReceiverMessage TCPReceiver::send() const
 {
   // Your code here.
-  uint16_t available = writer().available_capacity() > UINT16_MAX? static_cast<uint16_t>( UINT16_MAX ): static_cast<uint16_t>( writer().available_capacity() );
-  uint64_t ackno = writer().bytes_pushed()+1;
+  uint16_t available = writer().available_capacity() > UINT16_MAX? UINT16_MAX: uint16_t(writer().available_capacity());
+  uint64_t ackno = writer().bytes_pushed()+1+uint64_t(writer().is_closed());
   if(already_syn)
     return TCPReceiverMessage{Wrap32::wrap(ackno,isn_),available,writer().has_error()};
   else 
